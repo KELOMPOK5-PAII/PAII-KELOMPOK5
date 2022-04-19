6b3846a62fdc3bef69fc3da42310de4a99779a57
esteryb <esteryolandaberutu@gmail.com>
2022-04-19 11:27:00 +0700

    login2

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.app')
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="{{ url('sbadmin/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{ url('sbadmin/css/sb-admin-2.min.css')}}" rel="stylesheet">
+
+</head>
+
+    <body>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-6">
+            <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+            <div class="row">
+            <div class="col-lg">
+            <div class="p-5">
+            <div class="text-center">
+            <h1 class="h4 text-gray-900">LOGIN</h1>
+                                        <p>Sistem Informasi Fasilitas IT Del</p>
+                                    </div>
+
+                                    @if(session('error'))
+                                    <div class="alert alert-danger">
+                                        <b>Opps!</b> {{session('error')}}
+                                    </div>
+                                    @endif
+
+
+                    <form method="POST" class="user" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group">
+
+                                <input id="email" type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus aria-describedby="emailHelp" placeholder="Enter Email Address..." required="">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
+
+                            </div>
+
+
+
+
+                            <div class="form-group">
+                                <input id="password" type="password" class="form-control form-control-user @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password" required="">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+
+                        <div class="row mb-3">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+
+                                <button type="submit" class="btn btn-primary btn-user btn-block">
+                                    {{ __('Login') }}
+                                </button>
+
+
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+</div>
+</body>
+@endsection
