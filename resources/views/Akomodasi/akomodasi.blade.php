6559d61776ca7d44f07f362c4b38f753cad9aea9
Srinesia28 <srinesiasitorus118@gmail.com>
2022-06-16 19:49:01 +0700

    perbaikan

--- resources/views/Akomodasi/akomodasi.blade.php
+++ resources/views/Akomodasi/akomodasi.blade.php
@@ -17,8 +17,8 @@
             <div class="row">
                     <div class="col-1 text-light" style= "margin-left:100px;"><H1>Akomodasi</H1>
                     <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="500" height="500" preserveAspectRatio="xMidYMid meet" viewBox="0 0 576 512"><path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"/></svg></div>
-                    <div class="col-8 text-light" style= "margin-top:200px; padding-left:550px">
-                    <p style= "font-size: 20px; ">Institut Teknologi Del menyediakan akomodasi untuk mahasiswa, dosen dan tamu. Akomodasi di IT Del dilengkapi dengan furniture yang lengkap dan didukung oleh pemandangan indah panoraman Danau Toba menambah keasrian. Akomodasi sebagai Fasilitas di IT Del diharapkan memberikan kenyamanan bagi yang menempati.</p>
+                    <div class="col-9 text-light" style= "margin-top:150px; padding-left:550px">
+                    <p style= "font-size: 23px; ">Institut Teknologi Del menyediakan akomodasi untuk mahasiswa, dosen dan tamu. Akomodasi di IT Del dilengkapi dengan furniture yang lengkap dan didukung oleh pemandangan indah panoraman Danau Toba menambah keasrian. Akomodasi sebagai Fasilitas di IT Del diharapkan memberikan kenyamanan bagi yang menempati.</p>
                     </div>
                     </div>
 
--- resources/views/Denah/denah.blade.php
+++ resources/views/Denah/denah.blade.php
@@ -19,8 +19,8 @@
                     <div class="col-1 text-light" style= "margin-left:100px; margin-bottom:50px;"><H1>Denah</H1> 
                     <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="400" height="400" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512"><path fill="currentColor" d="M507.31 84.69L464 41.37c-6-6-14.14-9.37-22.63-9.37H288V16c0-8.84-7.16-16-16-16h-32c-8.84 0-16 7.16-16 16v16H56c-13.25 0-24 10.75-24 24v80c0 13.25 10.75 24 24 24h385.37c8.49 0 16.62-3.37 22.63-9.37l43.31-43.31c6.25-6.26 6.25-16.38 0-22.63zM224 496c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V384h-64v112zm232-272H288v-32h-64v32H70.63c-8.49 0-16.62 3.37-22.63 9.37L4.69 276.69c-6.25 6.25-6.25 16.38 0 22.63L48 342.63c6 6 14.14 9.37 22.63 9.37H456c13.25 0 24-10.75 24-24v-80c0-13.25-10.75-24-24-24z"/></svg>  
                   </div>   
-                    <div class="col-8 text-light" style= "margin-top:100px; padding-left:450px">
-                    <p style= "font-size: 20px; ">Denah merupakan gambaran dari tata letak, lokasi, atau tempat suatu bangunan secara spesifik yang dapat membantu dalam memudahkan seseorang menemukan lokasi yang dituju dengan disertai keterangan. Berikut adalah gambaran dari Denah IT Del beserta keterangan tempat :</p>
+                    <div class="col-8 text-light" style= "margin-top:130px; padding-left:450px">
+                    <p style= "font-size: 23px; ">Denah merupakan gambaran dari tata letak, lokasi, atau tempat suatu bangunan secara spesifik yang dapat membantu dalam memudahkan seseorang menemukan lokasi yang dituju dengan disertai keterangan. Berikut adalah gambaran dari Denah IT Del beserta keterangan tempat :</p>
                     </div>                 
                 </div>
             </div>
